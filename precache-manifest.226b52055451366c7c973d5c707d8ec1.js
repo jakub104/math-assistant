f76ae1716b8c3786eb916c13a4150255b4c38748
jakub104 <jakubzlezena@wp.pl>
2019-08-07 10:31:15 +0200

    Updates

---
+++ precache-manifest.226b52055451366c7c973d5c707d8ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c12bc6db458dc5cbbf08bc8b67b4f5",
+    "url": "/math-assistant/index.html"
+  },
+  {
+    "revision": "88da94a5de0f118c105f",
+    "url": "/math-assistant/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/math-assistant/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "88da94a5de0f118c105f",
+    "url": "/math-assistant/static/js/main.e2143db8.chunk.js"
+  },
+  {
+    "revision": "dab880646999a201ff51",
+    "url": "/math-assistant/static/js/runtime~main.6674d726.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/math-assistant/static/media/logo.5d5d9eef.svg"
+  }
+]);
